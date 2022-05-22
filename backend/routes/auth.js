e8ac796030b6ff59cf5318476028b34828599b0e
Sahil-pillania <sahilpillania88@gmail.com>
2022-05-22 12:09:27 +0530

    thunderClient collection add and some validation checks

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,36 +3,54 @@ const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
 
-// Create a user using POST "/api/auth/" . Doesn't require Auth
+// Create a user using POST "/api/auth/createUser" . No login required
 router.post(
-  "/",
+  "/createUser",
   [
     body("name", "Enter a valid name").isLength({ min: 3 }),
     body("email", "Enter a valid email").isEmail(),
     body("password").isLength({ min: 5 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     console.log(req.body);
 
+    // if there are errors return bad request and errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    })
-      .then((user) => res.json(user))
-      .catch((err) => {
-        console.log("Error occured !");
-        res.json({
-          error: "Please enter a unique value for email ",
-          message: err.message,
+    // check whether the user exists already \\
+
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "Sorry a user with this email already exists" });
+      } else {
+        user = await User.create({
+          name: req.body.name,
+          email: req.body.email,
+          password: req.body.password,
         });
-      });
-    // res.send(req.body);
+      }
+      res.json({ user });
+      // .then((user) => res.json(user))
+      // .catch((err) => {
+      //   console.log("Error occured !");
+      //   res.json({
+      //     error: "Please enter a unique value for email ",
+      //     message: err.message,
+      //   });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Some error occured !");
+    }
   }
 );
+
+// res.send(req.body);
+//   }
+// );
 
 module.exports = router;
