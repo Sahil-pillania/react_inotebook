6004d3159403c9665effd566649119ebfcdd1640
Sahil-pillania <sahilpillania88@gmail.com>
2022-05-22 21:29:20 +0530

    Hashing the password using bcrytpjs and JWT token generate

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
 
+const bcrypt = require("bcryptjs");
+const JWT_SECRET = "Sahilthedeveloper";
+var jwt = require("jsonwebtoken");
+
 // Create a user using POST "/api/auth/createUser" . No login required
 router.post(
   "/createUser",
@@ -27,14 +31,29 @@ router.post(
         return res
           .status(400)
           .json({ error: "Sorry a user with this email already exists" });
-      } else {
-        user = await User.create({
-          name: req.body.name,
-          email: req.body.email,
-          password: req.body.password,
-        });
       }
-      res.json({ user });
+
+      // Password hashing
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
+
+      // Create a neww user
+      user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: secPass,
+      });
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const jwtToken = jwt.sign(data, JWT_SECRET);
+      console.log(jwtToken);
+
+      res.json({ jwtToken });
+
+      //res.json({ user });
       // .then((user) => res.json(user))
       // .catch((err) => {
       //   console.log("Error occured !");
