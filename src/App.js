4198892d2fe58493233b352afac4befc19f62c84
Sahil-pillania <sahilpillania88@gmail.com>
2022-05-28 23:08:23 +0530

    created the components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-// import logo from "./logo.svg";
 import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
 
 function App() {
   return (
-    <div className="App">
-      <h2> App started 👍 </h2>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" component={<Home />} />
+          <Route exact path="/About" component={<About />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
