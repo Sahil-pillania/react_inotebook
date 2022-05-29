13d60008f4ed4ad7eaea0e0b99ce0cb387e5ab14
Sahil-pillania <sahilpillania88@gmail.com>
2022-05-29 11:26:50 +0530

    context api initialised

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
+import NoteState from "./context/notes/NoteState";
 
 function App() {
   return (
     <>
-      <Router>
-        <Navbar />
-        <Home />
-        <Routes>
-          <Route exact path="/" component={<Home />} />
-          <Route exact path="/About" component={<About />} />
-        </Routes>
-      </Router>
+      <NoteState>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/About" element={<About />} />
+          </Routes>
+        </Router>
+      </NoteState>
     </>
   );
 }
--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import noteContext from "../context/notes/NoteContext";
 
 const About = () => {
-  return <div>About</div>;
+  const a = useContext(noteContext);
+  useEffect(() => {
+    a.update();
+    // eslint-disable-next-line
+  }, []);
+
+  return (
+    <>
+      <div>
+        {" "}
+        This is about {a.state.name} and class is {a.state.class}
+      </div>
+      <h2>Sahil pillania</h2>
+    </>
+  );
 };
 
 export default About;
